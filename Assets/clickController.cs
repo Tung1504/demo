abca4198acd9a54bebada821adcdeeff5d96eb28
Tung <natsu1504@gmail.com>
2020-08-18 16:47:51 +0700

    Add WorkCookie

---
+++ Assets/clickController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickController : MonoBehaviour
+{
+   
+
+    void OnMouseDown()
+    {
+        gameMaster.currentWord += GetComponent<TextMesh>().text;
+        gameMaster.letterNum += 1;
+        gameMaster.selectLetter[gameMaster.letterNum] = GetComponent<TextMesh>().text;
+    }
+}
---
+++ Assets/gameMaster.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMaster : MonoBehaviour
+{
+    public static string currentWord;
+    public Transform spelledWord;
+
+    public Transform letter1;
+    public Transform letter2;
+    public Transform letter3;
+
+    public Transform bttLetter1;
+    public Transform bttLetter2;
+    public Transform bttLetter3;
+    //public Transform bttLetter4;
+
+    public static List<string> availLetter = new List<string>() { "W", "I", "N", "E" };
+
+    public KeyCode RMB;
+    public int wordLen;
+    public string word3L = "WIN";
+    public static List<string> selectLetter = new List<string>() {"","","",""};
+    public static int letterNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bttLetter1.GetComponent<TextMesh>().text = availLetter[0];
+        bttLetter2.GetComponent<TextMesh>().text = availLetter[1];
+        bttLetter3.GetComponent<TextMesh>().text = availLetter[2];
+        //bttLetter4.GetComponent<TextMesh>().text = availLetter[3];
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spelledWord.GetComponent<TextMesh>().text = currentWord;
+
+        if (Input.GetKeyDown(RMB))
+        {
+            wordLen = currentWord.Length;
+
+
+
+
+            if (wordLen == 3)
+            {
+                if (currentWord == word3L)
+                {
+                    letter1.GetComponent<TextMesh>().text = selectLetter[1];
+                    letter2.GetComponent<TextMesh>().text = selectLetter[2];
+                    letter3.GetComponent<TextMesh>().text = selectLetter[3];
+                }
+                else
+                {
+                    Debug.Log("Wrong word");
+                }
+            }
+        }
+
+        
+
+    }
+}
